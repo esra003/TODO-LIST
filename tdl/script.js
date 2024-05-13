186caf0f3517b37cc3d0d7a256656ad49a080722
Esra Serdarli <serdarliesra@gmail.com>
2024-05-13 14:30:15 +0400

    first commit

---
+++ tdl/script.js
@@ -0,0 +1,4 @@
+const addButton = document.querySelector('button');
+const input = document.getElementById('input');
+const plus = document.querySelector('.plus');
+const cleanİnput = document.querySelector('.delete');
